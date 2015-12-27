8f0e7a5fc9ba619c64364c0e972932db1eba3fd8
phpyatsu <php@yatsukouji.com>
2015-12-28 07:25:38 +0900

    レストラン登録機能の実装

--- app/Controller/ShopsController.php
+++ app/Controller/ShopsController.php
@@ -10,7 +10,19 @@ $this->log($this->Shop->find('all'));
     }
 
     public function view ($id)
-{
+    {
     $this->set('data', $this->Shop->findById($id));
-}
+    }
+    public function add ()
+    {
+        if ($this->request->is('post'))
+        {
+            $this->Shop->set($this->data);
+            if ($this->Shop->validates())
+            {
+                $this->Shop->save($this->data);
+                $this->redirect('index');
+            }
+        }
+    }
 }
--- app/Model/Shop.php
+++ app/Model/Shop.php
@@ -2,5 +2,20 @@
 
 class Shop extends AppModel
 {
-
+    public $validate = array(
+        'name' => array(
+            'rule' => array('notBlank')
+        ),
+        'tel' => array(
+            'rule' => array('notBlank')
+        ),
+        'addr' => array(
+            'rule' => array('notBlank')
+        ),
+        'url' => array(
+            'rule' => array('url'),
+        //    'rule' => array('url', true) trueを置く?
+            'message' => '形式が正しくありません'
+            )
+        );
 }
